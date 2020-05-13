6090aa1a65d4b90bad60ca41716280d8f99c5dbd
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-13 17:14:25 +0530

    Created model manager

--- customusermodel/accounts/models.py
+++ customusermodel/accounts/models.py
@@ -7,6 +7,29 @@ from django.contrib.auth.models import(
 )
 
 
+class userManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError("User must have a emaill address")
+        if not password:
+            raise ValueError("User must have a password")
+        user_obj = self.model(email=self.normalize_email(email))
+        user_obj.set_password(password)
+        user_obj.admin = is_admin
+        user_obj.staff = is_staff
+        user_obj.active = is_active
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(email, password=password, is_staff=True)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password=password, is_admin=True)
+        return user
+
+
 class myUser(AbstractBaseUser):
     email = models.EmailField(max_length=300, unique=True)
     full_name = models.CharField(max_length=255, blank=True, null=True)
@@ -18,6 +41,8 @@ class myUser(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     # username an password are provided by default
     REQUIRED_FIELD = []
+
+    objects = userManager()
 
     def __str__(self):
         return
