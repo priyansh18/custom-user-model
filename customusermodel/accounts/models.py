0c728a7f41f6a6892f0060cf86ae630e03ab22de
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-13 12:37:07 +0530

    Creating abstract base user

--- customusermodel/accounts/models.py
+++ customusermodel/accounts/models.py
@@ -8,4 +8,34 @@ from django.contrib.auth.models import(
 
 
 class myUser(AbstractBaseUser):
-    username = models.CharField(max_length=300)
+    email = models.EmailField(max_length=300, unique=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    # username an password are provided by default
+    REQUIRED_FIELD = []
+
+    def __str__(self):
+        return
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.active
+
+    @property
+    def is_staff(self):
+        return self.staff
